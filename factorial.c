5d18c3fdd116664c106f5c22f156a64b29f5fef1
Oskkyyy <oscar.ortizb29@gmail.com>
2022-11-15 17:55:24 -0600

    Se creó el programa para calcular factoriales.

---
+++ factorial.c
@@ -0,0 +1,25 @@
+// Funcionamiento de un factorial
+#include <stdio.h>
+int factorial (int n)
+{
+	long int f;
+	if(n==0)
+	{
+	       	f=1;
+	}
+	else
+	{	
+		f=n*factorial(n-1);
+	}
+	return f;
+}
+int main()
+{
+	int fact;
+	int n;
+	printf ("\n Dame un numero:");
+	scanf("%d",&n);
+	fact=factorial(n);
+	printf("\n El factorial =%d \n",fact);
+	return 0;
+}
